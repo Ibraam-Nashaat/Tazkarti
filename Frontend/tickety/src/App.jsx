a3ce8e4cf9c7b92002876ed6fb88d54d1378494d
ibraam <ibraam.nashaat2@gmail.com>
2024-12-14 20:03:02 +0200

    Merge changes

--- Frontend/tickety/src/App.jsx
+++ Frontend/tickety/src/App.jsx
@@ -4,24 +4,22 @@ import WelcomePage from './pages/welocmePage/welcomePage';
 import SignInPage from './pages/signinPage/signInPage';
 import SignupPage from './pages/signupPage/signUpPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EditData from './pages/editDataPage/editData'
-import AddMatch from './pages/addMatchPage/addMatchPage'
-import ViewMatchPage from './pages/viewMatchesPage/ViewMatchPage'
+import EditData from './pages/editDataPage/editData';
+import AddMatch from './pages/addMatchPage/addMatchPage';
+import ViewMatchPage from './pages/viewMatchesPage/ViewMatchPage';
 function App() {
-  
-
   return (
     <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<WelcomePage />} />
-                    <Route  path="/signin" element= {<SignInPage/>} /> 
-                    <Route path="/signup" element={<SignupPage/>} />
-                    <Route   path ="/editdata"  element={<EditData />} />
-                    <Route   path="/addmatch"  element={<AddMatch />} />
-                    <Route   path="/viewmatch"  element={<ViewMatchPage />} />
-                </Routes>
-            </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/editdata" element={<EditData />} />
+          <Route path="/addmatch" element={<AddMatch />} />
+          <Route path="/viewmatch" element={<ViewMatchPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
